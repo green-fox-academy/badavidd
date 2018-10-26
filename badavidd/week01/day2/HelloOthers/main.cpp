169eef867932d187267a05b2a11da065fe279ff1
badavidd <43615638+badavidd@users.noreply.github.com>
2018-10-26 13:23:45 +0200

    helloothers

---
+++ badavidd/week01/day2/HelloOthers/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main(int argc, char const *argv[]) {
+
+    // Greet 3 of your classmates with this program, in three separate lines
+    // like:
+    //
+    // Hello, Esther!
+    // Hello, Mary!
+    // Hello, Joe!
+
+    std::cout << "Hello, Marci!" << std::endl;
+    std::cout << "Hello, Zsuzsi!" << std::endl;
+    std::cout << "Hello, Roland!" << std::endl;
+
+    return 0;
+}
