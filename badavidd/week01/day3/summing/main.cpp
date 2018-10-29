cc22c5e44afcb8c8cf5a57676662ec4bd6956df6
badavidd <43615638+badavidd@users.noreply.github.com>
2018-10-29 14:51:56 +0100

    summing

---
+++ badavidd/week01/day3/summing/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+
+int sum(int number) {
+    int sum = 0;
+    for (int i = 0; i < number; i++)
+        sum += i;
+    return sum;
+}
+int main(int argc, char* args[]) {
+
+    // - Write a function called `sum` that sum all the numbers
+    //   until the given parameter and returns with an integer
+
+int baseNum = 10;
+std::cout << sum(baseNum) << std::endl;
+
+    return 0;
+}
