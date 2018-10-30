73937afde6bf0cb74ca453c1be325ab031edb025
badavidd <43615638+badavidd@users.noreply.github.com>
2018-10-30 14:39:59 +0100

    whatev

---
+++ badavidd/week01/day3/appenda/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+std::string append(std::string x)
+{
+  std::string result = x + "a";
+  return result;
+}
+
+int main() {
+
+    std::string animals[3]{"koal", "pand", "zebr"};
+
+    for(int i = 0; i < (sizeof(animals) / sizeof(animals[3])); i++)
+
+    return 0;
+}
---
+++ badavidd/week01/day3/colors/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+int main() {
+
+    std::string colors[]{"lime", "forest green", "olive", "pale green", "spring green"};
+    std::string colors[]{"orange red", "red", "tomato"};
+    std::string colors[]{"orchid", "violet", "pink", "hot pink"};
+
+
+
+    return 0;
+}
