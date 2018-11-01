95afa92d198865c21281df1963ca2062915c6476
badavidd <43615638+badavidd@users.noreply.github.com>
2018-11-01 20:02:15 +0100

    quoteswap

---
+++ badavidd/week01/day5/quoteswap/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
+
+int main(int argc, char* args[])
+{
+    std::vector<std::string> quote = {"What", "I", "do", "create,", "I", "cannot", "not", "understand."};
+
+    // Accidentally I messed up this quote from Richard Feynman.
+    // Two words are out of place
+    // Your task is to fix it by swapping the right words with code
+    // Create a method called quoteSwap().
+
+    // Also, print the sentence to the output with spaces in between.
+
+    return 0;
+}
+
+std::vector<std::string> quoteSwap(std::vector<std::string> quote)
+{
+
+}
