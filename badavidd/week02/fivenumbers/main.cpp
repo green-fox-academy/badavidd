e9874677cc0da65e590c6b66fc32d8a218f32e80
badavidd <43615638+badavidd@users.noreply.github.com>
2018-11-06 13:35:13 +0100

    fivenumbers

---
+++ badavidd/week02/fivenumbers/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+int main()
+{
+    // Create a program which accepts five integers from the console (given by the user)
+    // and store them in an array
+    // print out the values of that array using pointers again
+
+    int a[5] = {};
+    int *aPtr = nullptr;
+
+    for(int i = 0; i < 5; i++){
+        std::cout << "Number pls!";
+        std::cin >> a[i];
+    }
+    for(int j = 0; j < 5; j++){
+        aPtr = &a[j];
+        std::cout << *aPtr << std::endl;
+    }
+    return 0;
+}
