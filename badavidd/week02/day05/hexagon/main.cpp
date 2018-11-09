c0515b20302adeba97891f33d3fc950c36c5cfb7
badavidd <43615638+badavidd@users.noreply.github.com>
2018-11-09 14:05:46 +0100

    hexa

--- badavidd/week02/day05/hexagon/main.cpp
+++ badavidd/week02/day05/hexagon/main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <SDL.h>
 #include <SDL2_gfxPrimitives.h>
+#include <vector>
+
 
 //Screen dimension constants
 const int SCREEN_WIDTH = 640;
@@ -16,63 +18,94 @@ bool init();
 void close();
 
 //The window we'll be rendering to
-SDL_Window* gWindow = nullptr;
+SDL_Window *gWindow = nullptr;
 
 //The window renderer
-SDL_Renderer* gRenderer = nullptr;
+SDL_Renderer *gRenderer = nullptr;
 
-void draw()
-{
+void draw() {
+    Sint16 a[] = {320, 340, 351, 340, 320, 309};
+    Sint16 b[] = {11, 11, 30, 49, 49, 30};
 
+    Sint16 *aPtr = a;
+    Sint16 *bPtr = b;
+    polygonRGBA(gRenderer, aPtr, bPtr, 6, 0x00, 0x00, 0x00, 0xFF);
+    for (int j = 0; j < 6; j++) {
+        for (int i = 0; i < 6; i++) {
+            b[i] = b[i] + 38;
+        }
+        aPtr = a;
+        bPtr = b;
+        polygonRGBA(gRenderer, aPtr, bPtr, 6, 0x00, 0x00, 0x00, 0xFF);
+
+    }
+    for(int r = 0; r < 6; r++) {
+        a[r] = a[r] - 39;
+    }
+//MARADÉKOS OSZTÁSSAL a 3. és 6. pontnak más az eltolása mint a többinek
+    for (int j = 0; j < 6; j++) {
+
+        for (int i = 0; i < 6; i++) {
+            b[i] = b[i] - 19;
+        }
+        aPtr = a;
+        bPtr = b;
+        polygonRGBA(gRenderer, aPtr, bPtr, 6, 0x00, 0x00, 0x00, 0xFF);
+
+    }
 }
 
-bool init()
-{
+/*
+ *  for (int k = 0; k < 1; k++) {
+                a[j] = a[j] - 38;
+                b[j] = b[j] + 38;
+            }
+            aPtr = a;
+            bPtr = b;
+            polygonRGBA(gRenderer, aPtr, bPtr, 6, 0x00, 0x00, 0x00, 0xFF);
+ */
+
+bool init() {
     //Initialize SDL
-    if( SDL_Init( SDL_INIT_VIDEO ) < 0 )
-    {
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
         std::cout << "SDL could not initialize! SDL Error: " << SDL_GetError() << std::endl;
         return false;
     }
 
     //Create window
-    gWindow = SDL_CreateWindow( "Starry night", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
-    if( gWindow == nullptr )
-    {
+    gWindow = SDL_CreateWindow("Starry night", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH,
+                               SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+    if (gWindow == nullptr) {
         std::cout << "Window could not be created! SDL Error: " << SDL_GetError() << std::endl;
         return false;
     }
 
     //Create renderer for window
-    gRenderer = SDL_CreateRenderer( gWindow, -1, SDL_RENDERER_ACCELERATED );
-    if( gRenderer == nullptr )
-    {
+    gRenderer = SDL_CreateRenderer(gWindow, -1, SDL_RENDERER_ACCELERATED);
+    if (gRenderer == nullptr) {
         std::cout << "Renderer could not be created! SDL Error: " << SDL_GetError() << std::endl;
         return false;
     }
 
     //Initialize renderer color
-    SDL_SetRenderDrawColor( gRenderer, 0xFF, 0xFF, 0xFF, 0xFF );
+    SDL_SetRenderDrawColor(gRenderer, 0xFF, 0xFF, 0xFF, 0xFF);
 
     return true;
 }
 
-void close()
-{
+void close() {
     //Destroy window
-    SDL_DestroyRenderer( gRenderer );
-    SDL_DestroyWindow( gWindow );
+    SDL_DestroyRenderer(gRenderer);
+    SDL_DestroyWindow(gWindow);
     gWindow = nullptr;
     gRenderer = nullptr;
 
     SDL_Quit();
 }
 
-int main( int argc, char* args[] )
-{
+int main(int argc, char *args[]) {
     //Start up SDL and create window
-    if( !init() )
-    {
+    if (!init()) {
         std::cout << "Failed to initialize!" << std::endl;
         close();
         return -1;
@@ -85,7 +118,7 @@ int main( int argc, char* args[] )
     SDL_Event e;
 
     //While application is running
-    while( !quit ) {
+    while (!quit) {
         //Handle events on queue
         while (SDL_PollEvent(&e) != 0) {
             //User requests quit
