f114f3647d0ff6a91243bb5bb301e4b5ba72af51
badavidd <43615638+badavidd@users.noreply.github.com>
2018-11-12 14:15:44 +0100

    copy

---
+++ badavidd/week03/day01/copyfile/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+// Write a function that reads all lines of a file and writes the read lines to an other file (a.k.a copies the file)
+// It should take the filenames as parameters
+// It should return a boolean that shows if the copy was successful
+
+bool copy(std::string, std::string);
+
+int main() {
+
+    std::string file = {"original.txt"};
+    std::string newfile = {"copied.txt"};
+
+    std::cout << copy(file, newfile) << std::endl;
+
+    return 0;
+}
+
+bool copy(std::string file, std::string newfile) {
+    std::ifstream file2("../" + file);
+    std::ofstream newfile2("../" + newfile);
+    std::string line;
+    if (file2.is_open() || newfile2.is_open()) {
+        while (std::getline(file2, line))
+            newfile2 << line << std::endl;
+    } else {
+        return false;
+    }
+    return true;
+}
