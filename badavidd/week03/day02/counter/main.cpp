59ff5749b9e557c6542027e5879ae3c6195fe8a7
badavidd <43615638+badavidd@users.noreply.github.com>
2018-11-14 11:20:39 +0100

    counter2

--- badavidd/week03/day02/counter/main.cpp
+++ badavidd/week03/day02/counter/main.cpp
@@ -1,20 +1,35 @@
 #include <iostream>
 #include "counter.h"
 
+void checkValue(const std::string& name, int expected, int actual)
+{
+    if(expected != actual) {
+        std::cout << name << "Failed! Expected: " << expected << " Actual: " << actual << std::endl;
+    }
+    else {
+        std::cout << name << "Passed!" << std::endl;
+    }
+}
 
-int main() {
+int main(int argc, char* args[])
+{
+    Counter c;
+    checkValue("getZero", 0, c.get());
 
-    Counter counter;
-    Counter counter1(10);
-    counter1.add();
-    std::cout << counter1.get() << std::endl;
-    counter.add();
-    std::cout << counter.get() << std::endl;
-    counter1.add(5);
-    counter.add(3);
-    std::cout << counter1.get() << " " << counter.get() << std::endl;
-    counter.reset();
-    counter1.reset();
-    std::cout << counter1.get() << " " << counter.get() << std::endl;
+    c.add(5);
+    checkValue("addMore", 5, c.get());
+
+    c.add();
+    checkValue("addOne", 6, c.get());
+
+    c.reset();
+    checkValue("getZero2", 0, c.get());
+
+    Counter c2(7);
+    checkValue("getInit", 7, c2.get());
+
+    c2.reset();
+    checkValue("reset", 7, c2.get());
+
     return 0;
 }
