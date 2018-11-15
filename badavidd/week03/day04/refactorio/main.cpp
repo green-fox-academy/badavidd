0b0691bfa53b503bb8ee4f5dbdce274ba6fc9989
badavidd <43615638+badavidd@users.noreply.github.com>
2018-11-15 15:14:47 +0100

    refactorio

---
+++ badavidd/week03/day04/refactorio/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+// Create a recursive function called `refactorio`
+// that returns it's input's factorial
+
+int refactorio(int n) {
+    if (n > 0) {
+        return n * refactorio(n - 1);
+    } else {
+        return 1;
+    }
+}
+
+int main() {
+
+    std::cout << refactorio(5);
+
+    return 0;
+}
---
+++ badavidd/week03/day04/strings/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+// Given a string, compute recursively (no loops) a new string where all the
+// lowercase 'x' chars have been changed to 'y' chars.
+
+std::string change(std::string word)
+{
+
+}
+
+int main() {
+
+    std::cout <<change("xxx");
+
+    return 0;
+}
