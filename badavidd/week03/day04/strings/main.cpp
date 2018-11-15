3ff52080aa39efe9431203f9bb6ff024442de7c5
badavidd <43615638+badavidd@users.noreply.github.com>
2018-11-15 17:15:40 +0100

    string

--- badavidd/week03/day04/strings/main.cpp
+++ badavidd/week03/day04/strings/main.cpp
@@ -8,15 +8,15 @@ std::string change(std::string &word, int n) {
     if (n < word.size()) {
         if (word[n] == 'x')
             word[n] = 'y';
-            return change(word, n + 1);
-        } else {
-            return word;
-        }
+        return change(word, n + 1);
+    } else {
+        return word;
     }
+}
 
 
 int main() {
-    std::string thing = {"xxxaaaxxxbbbxxx"};
+    std::string thing = {"xxxaaaxxxbbbxxxcccxxx"};
     std::cout << change(thing, 0);
 
     return 0;
