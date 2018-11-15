66400a5993eaa5c7417b365a04e1dd34b3bd56fc
badavidd <43615638+badavidd@users.noreply.github.com>
2018-11-15 16:42:42 +0100

    string

--- badavidd/week03/day04/strings/main.cpp
+++ badavidd/week03/day04/strings/main.cpp
@@ -4,14 +4,20 @@
 // Given a string, compute recursively (no loops) a new string where all the
 // lowercase 'x' chars have been changed to 'y' chars.
 
-std::string change(std::string word)
-{
+std::string change(std::string &word, int n) {
+    if (n < word.size()) {
+        if (word[n] == 'x')
+            word[n] = 'y';
+            return change(word, n + 1);
+        } else {
+            return word;
+        }
+    }
 
-}
 
 int main() {
-
-    std::cout <<change("xxx");
+    std::string thing = {"xxxaaaxxxbbbxxx"};
+    std::cout << change(thing, 0);
 
     return 0;
 }
